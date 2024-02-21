ca6c0ac589372a3e5aeae6d0f0e2062f5a1ad78a
Saroj Kumar <sarojkum822@gmail.com>
2024-02-21 18:09:11 +0530

    moving_cards_features

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,26 +1,37 @@
 import React from 'react'
 import { SiConstruct3 } from "react-icons/si";
 import { FaDownload } from "react-icons/fa";
+import { IoClose } from "react-icons/io5";
+import { motion } from "framer-motion"
 
 
-const Card = ({ data }) => {
+const Card = ({ data, reference }) => {
     return (
-        <div className='relative text-white w-60 h-72 bg-zinc-900/90 px-8 py-10 rounded-[30px] overflow-hidden'>
+        <motion.div drag dragConstraints={reference} className='relative text-white w-60 h-72 bg-zinc-900/90 px-8 py-10 rounded-[30px] overflow-hidden'>
             <SiConstruct3 />
-            <p className='text-xs mt-6 font-semibold leading-tight'>{data.desc}</p>
+            <div className='mt-3 rounded-full justify-center items-center'>
+                {data.image && <img src={data.image} alt="Card Image" className="mb-4" />}
+            </div>
+            <p className='text-xs mt-1 font-semibold leading-tight'>{data.desc}</p>
             <div className='absolute footer bottom-0  w-full left-0'>
-                <div className='flex items-center justify-between px-5 py-3 mb-3'>
+                <div className='flex items-center justify-between px-7 py-1 mb-1'>
                     <h5>{data.filesize}</h5>
                     <span className='w-7 h-7 bg-zinc-600 rounded-full flex justify-center items-center'>
-                        <FaDownload size=".8em" color='#fff' />
+                        {data.close ? <IoClose /> : <FaDownload size=".8em" color='#fff' />}
                     </span>
                 </div>
-                <div className='tag w-full py-2 bg-${data.tag.tagColor} flex justify-center allign-center bg-green-800'>
-                    <h3 className='text-sm'>Download now</h3>
-                </div>
+                {
+                    data.tag.isOpen && (
+
+                        <div className={`tag w-full py-2 ${data.tag.tagColor === "blue" ? "bg-blue-800" : "bg-green-800"} flex justify-center align-center`}>
+                            <h3 className='text-sm'>{data.tag.tagtitle}</h3>
+                        </div>
+                    )
+                }
+
             </div>
 
-        </div>
+        </motion.div>
 
     )
 }
--- src/components/Foreground.jsx
+++ src/components/Foreground.jsx
@@ -1,25 +1,47 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Card from './Card'
+import img1 from '../assets/img1.jpeg'
+import img2 from '../assets/img2.jpeg'
 
 const Foreground = () => {
     // const data=[
     //     desc,filesize,closeOrDownload,tagDetails
     // ];
 
+    const ref = useRef(null);
+
     const data = [
         {
             desc: "This is a description",
+            image: img1,
             filesize: ".9mb",
             close: true,
-            tag: { isOpen: true, tagtitle: "download Now", tagColor: "blue" },
+            tag: { isOpen: true, tagtitle: "Upload Now", tagColor: "orange" },
+        },
+        {
+            desc: "This is a description of 2nd card",
+            image: img2,
+            filesize: ".19mb",
+            close: true,
+            tag: { isOpen: true, tagtitle: "download Now", tagColor: "green" },
         }
+
+        ,
+        {
+            desc: "This is a description of 3nd card",
+            image: img1,
+            filesize: ".119mb",
+            close: true,
+            tag: { isOpen: true, tagtitle: "download Now", tagColor: "yellow" },
+        }
+
     ];
 
     return (
         <>
-            <div className='fixed top-0 left-0 z-[3] w-full h-screen '>
+            <div ref={ref} className='fixed top-0 left-0 z-[3] w-full h-screen flex gap-5 p-10 flex-wrap'>
                 {data.map((item, index) => (
-                    <Card data={item} />
+                    <Card data={item} reference={ref} />
                 ))}
             </div>
 
